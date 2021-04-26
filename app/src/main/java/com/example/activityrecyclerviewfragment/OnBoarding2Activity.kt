2bbaf8513d8fbbddfa5d5d5b78579f7f42ebd6db
minhlqd <maiminhlqd@gmail.com>
2021-04-26 22:24:03 +0700

    On boarding

--- app/src/main/java/com/example/activityrecyclerviewfragment/OnBoarding2Activity.kt
+++ app/src/main/java/com/example/activityrecyclerviewfragment/OnBoarding2Activity.kt
@@ -1,24 +1,29 @@
 package com.example.activityrecyclerviewfragment
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_on_boarding_1.*
+import kotlinx.android.synthetic.main.activity_on_boarding_2.*
 
-class OnBoarding1Activity : AppCompatActivity() {
+class OnBoarding2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding_1)
+        setContentView(R.layout.activity_on_boarding_2)
 
         next.setOnClickListener {
             val intent = Intent(this, OnBoarding2Activity::class.java)
             startActivity(intent)
         }
 
-        skip.setOnClickListener {
-            Toast.makeText(this, "aaa",Toast.LENGTH_LONG).show()
+        back.setOnClickListener {
+            val intent = Intent(this, OnBoarding1Activity::class.java)
+            startActivity(intent)
         }
+
+        skip.setOnClickListener {
+            Toast.makeText(this, "aaa", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
